a96b375a1cac38265f4aae34db16453d815f4c6c
FentonPlusPlus <fentonhutson@gmail.com>
2023-01-25 11:46:47 +0000

    components added

---
+++ bae/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
---
+++ bae/src/components/TeamStats/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+import React from 'react'
+
+function TeamStats() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TeamStats
