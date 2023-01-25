3634ae1ff954b0ce8c3a498421e33999d07a11e9
Chet Jogia <chetjogia@gmail.com>
2023-01-25 13:07:43 +0000

    merged

--- bae/src/App.js
+++ bae/src/App.js
@@ -1,32 +1,30 @@
-import './App.css';
-import Form from './components/Form';
+import "./App.css";
+import Form from "./components/Form";
+import ProgressBar from "./components/ProgressBar";
+import { useState } from "react";
+import Header from "./components/Header"
 
 function App() {
+  const [progressValue, setProgressValue] = useState(50);
 
-  async function onClickHandler(data){
-    let dataObject = {points: data} 
-    
-    await fetch('https://bae.onrender.com/api/points/1' , {
+  async function onClickHandler(data) {
+    let dataObject = { points: data };
+
+    await fetch("https://bae.onrender.com/api/points/1", {
       method: "PATCH",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify(dataObject),
-    })
+    });
   }
 
-
-import ProgressBar from './components/ProgressBar';
-import { useState } from 'react';
-
-function App() {
-  const [progressValue, setProgressValue] = useState(50);
-  
   return (
     <div className="App">
+      <Header/>
       <ProgressBar progressValue={progressValue}></ProgressBar>
-
+      <Form onClickHandler={onClickHandler} />
     </div>
   );
 }
